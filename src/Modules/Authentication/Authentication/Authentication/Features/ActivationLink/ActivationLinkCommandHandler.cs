3084b6234d5ae466e7125fd49398ca7dd8e37bd4
“Pamela” <pamela@dev.al>
2025-03-25 14:53:10 +0100

    build error fixed

--- src/Modules/Authentication/Authentication/Authentication/Features/ActivationLink/ActivationLinkCommandHandler.cs
+++ src/Modules/Authentication/Authentication/Authentication/Features/ActivationLink/ActivationLinkCommandHandler.cs
@@ -1,16 +1,16 @@
 ﻿namespace Authentication.Authentication.Features.ActivationLink
 {
-    public record ActivationLinkCommand(User User) : IRequest<Results>;
+    public record ActivationLinkCommand(User User) : IRequest<Shared.Results.Results>;
 
-    public class ActivationLinkCommandHandler(IEmailService emailService, UserManager<User> _userManager) : IRequestHandler<ActivationLinkCommand, Results>
+    public class ActivationLinkCommandHandler(IEmailService emailService, UserManager<User> _userManager) : IRequestHandler<ActivationLinkCommand, Shared.Results.Results>
     {
-        public async Task<Results> Handle(ActivationLinkCommand request, CancellationToken cancellationToken)
+        public async Task<Shared.Results.Results> Handle(ActivationLinkCommand request, CancellationToken cancellationToken)
         {
             var token = await _userManager.GeneratePasswordResetTokenAsync(request.User);
             var encodedToken = WebUtility.UrlEncode(token);
             var message = $"Please click on the link to activate your account: https://localhost:5055/scalar/v1/set-password?userId={request.User.Id}&token={encodedToken}";
             await emailService.SendEmailAsync(request.User.Email, "GYM.Api Account Activation", message);
-            return Results.Success();
+            return Shared.Results.Results.Success();
         }
     }
 
